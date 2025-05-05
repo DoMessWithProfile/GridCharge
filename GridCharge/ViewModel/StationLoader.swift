ed584a89365afa22fe9ae9441d286a4b74c10864
DoMess <michael.lam14@outlook.com>
2025-05-05 11:25:10 +1000

    Sync to regular JSON file

--- GridCharge/ViewModel/StationLoader.swift
+++ GridCharge/ViewModel/StationLoader.swift
@@ -12,11 +12,13 @@ class StationLoader: ObservableObject {
     @Published var stations: [ChargingStation] = []
 
     init() {
-        loadStations()
+        load_ACStations()
+//        load_DCStations()
+//        load_UpcomingStations()
     }
 
-    func loadStations() {
-        guard let url = Bundle.main.url(forResource: "AC_Chargers2", withExtension: "JSON") else {
+    func load_ACStations() {
+        guard let url = Bundle.main.url(forResource: "AC_Chargers", withExtension: "JSON") else {
             print("JSON not found")
             return
         }
@@ -29,4 +31,36 @@ class StationLoader: ObservableObject {
             print("Error decoding JSON: \(error)")
         }
     }
+    
+    // FIXME: Require different Models due to different data for each Charger
+    // - Create a new model for each / make it adaptable
+//    func load_DCStations() {
+//        guard let url = Bundle.main.url(forResource: "DC_Chargers", withExtension: "JSON") else {
+//            print("JSON not found")
+//            return
+//        }
+//
+//        do {
+//            let data = try Data(contentsOf: url)
+//            let decoded = try JSONDecoder().decode([ChargingStation].self, from: data)
+//            self.stations = decoded
+//        } catch {
+//            print("Error decoding JSON: \(error)")
+//        }
+//    }
+//    
+//    func load_UpcomingStations() {
+//        guard let url = Bundle.main.url(forResource: "Upcoming_Chargers", withExtension: "JSON") else {
+//            print("JSON not found")
+//            return
+//        }
+//
+//        do {
+//            let data = try Data(contentsOf: url)
+//            let decoded = try JSONDecoder().decode([ChargingStation].self, from: data)
+//            self.stations = decoded
+//        } catch {
+//            print("Error decoding JSON: \(error)")
+//        }
+//    }
 }

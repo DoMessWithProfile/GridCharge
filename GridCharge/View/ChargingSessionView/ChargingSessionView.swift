02b8870ef7b1016d0071e647c277e532f60e89df
Benjamin Vu <117449255+ben-vu@users.noreply.github.com>
2025-05-13 01:20:06 +1000

    Update ChargingSessionView.swift

--- GridCharge/View/ChargingSessionView/ChargingSessionView.swift
+++ GridCharge/View/ChargingSessionView/ChargingSessionView.swift
@@ -70,6 +70,7 @@ struct ChargingSessionView: View {
             
             VStack {
                 Spacer()
+                // Navigation Bar
                 NavigationView()
                     .environmentObject(navigationViewModel)
             }

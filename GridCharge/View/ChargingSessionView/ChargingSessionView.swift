eb70fe44b7229103b9058fa978295fe303b02bde
Benjamin Vu <117449255+ben-vu@users.noreply.github.com>
2025-05-13 01:13:29 +1000

    Update ChargingSessionView.swift

--- GridCharge/View/ChargingSessionView/ChargingSessionView.swift
+++ GridCharge/View/ChargingSessionView/ChargingSessionView.swift
@@ -8,56 +8,79 @@
 import SwiftUI
 
 struct ChargingSessionView: View {
+    @StateObject private var navigationViewModel = NavigationViewModel()
+    
     var body: some View {
-        VStack {
-            HStack {
+        ZStack {
+            VStack {
+                HStack {
+                    Spacer()
+                    
+                    Text("Charge")
+                        .padding(.leading, 40)
+                    
+                    Spacer()
+                    
+                    Button {
+                        // action
+                    } label: {
+                        Image(systemName: "plus.circle.fill")
+                            .foregroundStyle(.purple)
+                    }
+                    .scaleEffect(1.5)
+                    .padding(.trailing)
+                }
+                
+                ZStack {
+                    Rectangle()
+                        .foregroundStyle(.regularMaterial)
+                    HStack {
+                        Spacer()
+                        Button {
+                            // action
+                        } label: {
+                            Text("SCHEDULE")
+                                .foregroundStyle(.black)
+                        }
+                        Spacer()
+                        Text("NOW")
+                        Spacer()
+                        Text("HISTORY")
+                        Spacer()
+                    }
+                }
+                .frame(height: 100)
+                
                 Spacer()
                 
-                Text("Charge")
-                    .padding(.leading, 40)
+                SessionView()
                 
+                Spacer()
                 Spacer()
                 
                 Button {
                     // action
                 } label: {
-                    Image(systemName: "plus.circle.fill")
-                        .foregroundStyle(.purple)
+                    T_ButtonView(buttonText: "End Session")
                 }
-                    .scaleEffect(1.5)
-                    .padding(.trailing)
+                Spacer()
+                Spacer()
+                Divider()
             }
-            ZStack {
-                Rectangle()
-                    .foregroundStyle(.regularMaterial)
-                HStack {
-                    Spacer()
-                    Button {
-                        // action
-                    } label: {
-                        Text("SCHEDULE")
-                            .foregroundStyle(.black)
-                    }
-                    Spacer()
-                    Text("NOW")
-                    Spacer()
-                    Text("HISTORY")
-                    Spacer()
-                }
+            
+            VStack {
+                Spacer()
+                NavigationView()
+                    .environmentObject(navigationViewModel)
             }
-            .frame(height: 80)
-            Spacer()
-            SessionView()
-            Spacer()
-            Button {
-                // action
-            } label: {
-                T_ButtonView(buttonText: "End Session")
-            }
-            Divider()
+        }
+        .navigationBarBackButtonHidden(true)
+        .onAppear {
+            navigationViewModel.currentTab = .charging
         }
     }
 }
+
 
 #Preview {
     ChargingSessionView()

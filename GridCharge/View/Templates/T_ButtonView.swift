b79b933680883af893d4af2b9cd581664ecd3541
Benjamin Vu <117449255+ben-vu@users.noreply.github.com>
2025-05-13 02:26:00 +1000

    Added shadow

--- GridCharge/View/Templates/T_ButtonView.swift
+++ GridCharge/View/Templates/T_ButtonView.swift
@@ -16,6 +16,7 @@ struct T_ButtonView: View {
             RoundedRectangle(cornerRadius: 15)
                 .frame(width: 280, height: 80)
                 .foregroundStyle(.purple)
+                .shadow(color: .black.opacity(0.5), radius: 5, x: 0, y: 4)
             Text("\(buttonText)")
                 .fontWeight(.heavy)
                 .foregroundStyle(.regularMaterial)

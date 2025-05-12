6c16ebb28885d910385dd41d1d48ea0c10a809f3
Benjamin Vu <117449255+ben-vu@users.noreply.github.com>
2025-05-13 01:12:53 +1000

    Update .MapView

--- GridCharge/View/MapView/MapView.swift
+++ GridCharge/View/MapView/MapView.swift
@@ -9,11 +9,9 @@ import SwiftUI
 import MapKit
 
 struct MapView: View {
-    
-    // TODO: JSON file read
+    @StateObject private var navigationViewModel = NavigationViewModel()
     
     var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: -33.88393, longitude: 151.20051), latitudinalMeters: 0.1, longitudinalMeters: 0.1)
-    
     
     var body: some View {
         NavigationStack {
@@ -22,55 +20,18 @@ struct MapView: View {
                 TestView()
                     .edgesIgnoringSafeArea(.all)
                 
-            // Bottom Navigation Bar
-            VStack(spacing: 0) {
-                Spacer()
-                HStack {
-                    Spacer()
-                    Button(action: {}) {
-                        Image(systemName: "dollarsign.circle")
-                            .font(.system(size: 24))
-                            .foregroundColor(.primary)
-                    }
-                    Spacer()
-                    Button(action: {}) {
-                        Image(systemName: "car")
-                            .font(.system(size: 24))
-                            .foregroundColor(.primary)
-                    }
-                    Spacer()
-                    Button(action: {}) {
-                        Image(systemName: "house")
-                            .font(.system(size: 24))
-                            .foregroundColor(.primary)
-                    }
-                    Spacer()
-                    NavigationLink(destination: ChargingSessionView()) {
-                        Image(systemName: "ev.charger")
-                            .font(.system(size: 24))
-                            .foregroundColor(.primary)
-                    }
-                    Spacer()
-                    Button(action: {}) {
-                        Image(systemName: "map")
-                            .font(.system(size: 24))
-                            .foregroundColor(.blue)
-                    }
-                    Spacer()
-                }
-                .padding(.vertical, 15)
-                .background(Color.white)
-                .shadow(radius: 2, y: -3)
-                }
-                .edgesIgnoringSafeArea(.bottom)
-                .zIndex(2) // Ensures navigation bar appears above all content
+                // Bottom Navigation Bar
+                NavigationView()
             }
-            .edgesIgnoringSafeArea(.bottom)
         }
-        .navigationBarHidden(true)
+        .navigationBarBackButtonHidden(true)
+        .environmentObject(navigationViewModel)
+        .onAppear {
+            navigationViewModel.currentTab = .map
+        }
     }
 }
-
+    
 #Preview {
     MapView()
 }
